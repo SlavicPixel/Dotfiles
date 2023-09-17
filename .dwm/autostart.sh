e5e4b8d6e7b4b0a1174e4a4c3dca24e9ab1dbc44
SlavicPixel <domi9132@gmail.com>
2023-09-17 17:38:40 +0200

    Updated alacritty config and dwm autostart script

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,12 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
 nitrogen --restore &
 numlockx on &
 dwmblocks &
-volumeicon &
-mullvad-vpn &
-cmst &
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+#volumeicon &
+#mullvad-vpn &
+#cmst &
+nm-applet &
+eddie-ui &
+#/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+lxpolkit &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
-/usr/bin/emacs --daemon &
+#/usr/bin/emacs --daemon &
 nvidia_pl &
+picom -b --experimental-backends &
